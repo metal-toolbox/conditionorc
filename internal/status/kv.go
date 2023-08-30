c0fb3b5e98449a4062ba82a1db6675f573731278
Doctor Vince <DoctorVin@users.noreply.github.com>
2023-08-30 18:59:36 -0400

    configure a status kv for each defined condition

--- internal/status/kv.go
+++ internal/status/kv.go
@@ -3,9 +3,11 @@ package status
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	ptypes "github.com/metal-toolbox/conditionorc/pkg/types"
+	"github.com/pkg/errors"
 
 	"github.com/nats-io/nats.go"
 	"github.com/sirupsen/logrus"
@@ -14,14 +16,19 @@ import (
 )
 
 var (
-	statusTTL         = 10 * 24 * time.Hour // expire status information in 10 days
-	firmwareInstallKV nats.KeyValue
+	statusOnce   sync.Once
+	statusTTL    = 10 * 24 * time.Hour // expire status information in 10 days
+	kvCollection = map[string]nats.KeyValue{}
+	errNotReady  = errors.New("condition kvs not initialized")
+	errNoKV      = errors.New("no kv for condition")
+	kvReady      bool
 )
 
 // ConnectToKVStores initializes all status KVs in preparation for monitoring status updates
 // Any errors here are fatal, as we are failing to initialize something we are explicitly
 // configured for.
-func ConnectToKVStores(s events.Stream, log *logrus.Logger, opts ...kv.Option) {
+func ConnectToKVStores(s events.Stream, log *logrus.Logger,
+	defs ptypes.ConditionDefinitions, opts ...kv.Option) {
 	js, ok := s.(*events.NatsJetstream)
 	if !ok {
 		log.Fatal("status via KV updates is only supported on NATS")
@@ -35,22 +42,49 @@ func ConnectToKVStores(s events.Stream, log *logrus.Logger, opts ...kv.Option) {
 		statusOpts = append(statusOpts, opts...)
 	}
 
-	var err error
-	firmwareInstallKV, err = kv.CreateOrBindKVBucket(js, string(ptypes.FirmwareInstall), statusOpts...)
-	if err != nil {
-		log.WithError(err).Fatal("unable to initialize NATS KV for firmware install")
-	}
-	//nolint:gocritic // shut up I know it's commented out.
-	/* inventoryKV, err := kv.CreateOrBindKV(js, string(ptypes.InventoryOutofband), opts...)
-		if err != nil {
-		log.WithErr(err).Fatal("unable to initialize NATS KV for inventory")
-	}*/
+	statusOnce.Do(func() {
+		for _, def := range defs {
+			kind := string(def.Kind)
+			hdl, err := kv.CreateOrBindKVBucket(js, kind, statusOpts...)
+			if err != nil {
+				log.WithError(err).
+					WithField("kv.type", kind).
+					Fatal("unable to initialize NATS KV for status")
+			}
+			kvCollection[kind] = hdl
+		}
+		kvReady = true
+	})
 }
 
-// WatchFirmwareInstallStatus specializes some generic NATS functionality, mainly to keep
+// WatchConditionStatus specializes some generic NATS functionality, mainly to keep
 // the callers cleaner of the NATS-specific details.
-func WatchFirmwareInstallStatus(ctx context.Context, facility string) (nats.KeyWatcher, error) {
+func WatchConditionStatus(ctx context.Context, kind ptypes.ConditionKind, facility string) (nats.KeyWatcher, error) {
+	if !kvReady {
+		return nil, errNotReady
+	}
+
+	kv, ok := kvCollection[string(kind)]
+	if !ok {
+		return nil, errors.Wrap(errNoKV, string(kind))
+	}
+
 	// format the facility as a NATS subject to use as a filter for relevant KVs
 	keyStr := fmt.Sprintf("%s.*", facility)
-	return firmwareInstallKV.Watch(keyStr, nats.Context(ctx))
+	return kv.Watch(keyStr, nats.Context(ctx))
+}
+
+// GetConditionKV returns the raw NATS KeyValue interface for the bucket associated
+// with the given condition type.
+func GetConditionKV(kind ptypes.ConditionKind) (nats.KeyValue, error) {
+	if !kvReady {
+		return nil, errNotReady
+	}
+
+	kv, ok := kvCollection[string(kind)]
+	if !ok {
+		return nil, errors.Wrap(errNoKV, string(kind))
+	}
+
+	return kv, nil
 }
