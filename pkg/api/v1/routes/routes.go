6d5e4612031d0a042a6b48d07d03c49254b86bc8
Alva Zhang <140438385+Alva8756@users.noreply.github.com>
2023-11-29 10:26:17 -0800

    remove duplicate delete

--- pkg/api/v1/routes/routes.go
+++ pkg/api/v1/routes/routes.go
@@ -139,10 +139,10 @@ func (r *Routes) Routes(g *gin.RouterGroup) {
 	if r.enableServerEnroll {
 		serverEnroll := g.Group("/serverEnroll")
 		serverEnroll.POST("/:uuid", r.composeAuthHandler(createScopes("server")), wrapAPICall(r.serverEnroll))
-		serverDelete := g.Group("/serverDelete")
-		serverDelete.DELETE("/:uuid", r.composeAuthHandler(createScopes("server")), wrapAPICall(r.serverDelete))
 		// Create a new server ID when uuid is not provided.
 		serverEnroll.POST("/", r.composeAuthHandler(createScopes("server")), wrapAPICall(r.serverEnroll))
+		serverDelete := g.Group("/server")
+		serverDelete.DELETE("/:uuid", r.composeAuthHandler(createScopes("server")), wrapAPICall(r.serverDelete))
 	}
 
 	servers := g.Group("/servers/:uuid")
