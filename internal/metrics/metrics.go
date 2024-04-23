4a17d4688a3fe3971932299bfa192cf4294355d3
Joel Rebello <jrebello@packet.com>
2024-04-23 17:08:57 +0200

    metrics: define a few additional metrics for collection

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -19,9 +19,13 @@ var (
 	apiLatencySeconds    *prometheus.HistogramVec
 	dependencyErrorCount *prometheus.CounterVec
 
-	ConditionQueued    *prometheus.CounterVec
-	ConditionCompleted *prometheus.CounterVec
-	PublishErrors      *prometheus.CounterVec
+	ConditionQueued         *prometheus.CounterVec
+	ConditionCompleted      *prometheus.CounterVec
+	PublishErrors           *prometheus.CounterVec
+	ConditionInKV           *prometheus.CounterVec
+	ConditionReconcileStale *prometheus.CounterVec
+
+	NatsKVUpdateEvent *prometheus.CounterVec
 )
 
 func init() {
@@ -73,6 +77,30 @@ func init() {
 			Help: "A counter metric to measure the total count of condition publish errors",
 		},
 		[]string{"conditionKind"},
+	)
+
+	ConditionInKV = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "conditionorc_condition_in_kv",
+			Help: "A count of all conditions listed in the KV.",
+		},
+		[]string{"conditionKind", "state"},
+	)
+
+	ConditionReconcileStale = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "conditionorc_condition_active_stale",
+			Help: "A count of active conditions identified to be stale.",
+		},
+		[]string{"conditionKind"},
+	)
+
+	NatsKVUpdateEvent = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "conditionorc_nats_kv_update_event",
+			Help: "A count of NATS KV Condition update events.",
+		},
+		[]string{"conditionKind", "valid"},
 	)
 }
 
