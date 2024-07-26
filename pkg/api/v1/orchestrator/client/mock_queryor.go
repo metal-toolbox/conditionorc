8b9fd878bcc62d2ac8a6db7ce0e79989e94bcd3b
Joel Rebello <jrebello@packet.com>
2024-07-26 08:10:53 +0200

    api/v1/orc: updated client mocks

--- pkg/api/v1/orchestrator/client/mock_queryor.go
+++ pkg/api/v1/orchestrator/client/mock_queryor.go
@@ -9,8 +9,6 @@ import (
 
 	mock "github.com/stretchr/testify/mock"
 
-	registry "github.com/metal-toolbox/rivets/events/registry"
-
 	types "github.com/metal-toolbox/conditionorc/pkg/api/v1/orchestrator/types"
 
 	uuid "github.com/google/uuid"
@@ -29,29 +27,29 @@ func (_m *MockQueryor) EXPECT() *MockQueryor_Expecter {
 	return &MockQueryor_Expecter{mock: &_m.Mock}
 }
 
-// ConditionQueuePop provides a mock function with given fields: ctx, conditionKind, serverID
-func (_m *MockQueryor) ConditionQueuePop(ctx context.Context, conditionKind condition.Kind, serverID uuid.UUID) (*types.ServerResponse, error) {
-	ret := _m.Called(ctx, conditionKind, serverID)
+// ConditionQuery provides a mock function with given fields: ctx, serverID
+func (_m *MockQueryor) ConditionQuery(ctx context.Context, serverID uuid.UUID) (*types.ServerResponse, error) {
+	ret := _m.Called(ctx, serverID)
 
 	if len(ret) == 0 {
-		panic("no return value specified for ConditionQueuePop")
+		panic("no return value specified for ConditionQuery")
 	}
 
 	var r0 *types.ServerResponse
 	var r1 error
-	if rf, ok := ret.Get(0).(func(context.Context, condition.Kind, uuid.UUID) (*types.ServerResponse, error)); ok {
-		return rf(ctx, conditionKind, serverID)
+	if rf, ok := ret.Get(0).(func(context.Context, uuid.UUID) (*types.ServerResponse, error)); ok {
+		return rf(ctx, serverID)
 	}
-	if rf, ok := ret.Get(0).(func(context.Context, condition.Kind, uuid.UUID) *types.ServerResponse); ok {
-		r0 = rf(ctx, conditionKind, serverID)
+	if rf, ok := ret.Get(0).(func(context.Context, uuid.UUID) *types.ServerResponse); ok {
+		r0 = rf(ctx, serverID)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*types.ServerResponse)
 		}
 	}
 
-	if rf, ok := ret.Get(1).(func(context.Context, condition.Kind, uuid.UUID) error); ok {
-		r1 = rf(ctx, conditionKind, serverID)
+	if rf, ok := ret.Get(1).(func(context.Context, uuid.UUID) error); ok {
+		r1 = rf(ctx, serverID)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -59,39 +57,38 @@ func (_m *MockQueryor) ConditionQueuePop(ctx context.Context, conditionKind cond
 	return r0, r1
 }
 
-// MockQueryor_ConditionQueuePop_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ConditionQueuePop'
-type MockQueryor_ConditionQueuePop_Call struct {
+// MockQueryor_ConditionQuery_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ConditionQuery'
+type MockQueryor_ConditionQuery_Call struct {
 	*mock.Call
 }
 
-// ConditionQueuePop is a helper method to define mock.On call
+// ConditionQuery is a helper method to define mock.On call
 //   - ctx context.Context
-//   - conditionKind condition.Kind
 //   - serverID uuid.UUID
-func (_e *MockQueryor_Expecter) ConditionQueuePop(ctx interface{}, conditionKind interface{}, serverID interface{}) *MockQueryor_ConditionQueuePop_Call {
-	return &MockQueryor_ConditionQueuePop_Call{Call: _e.mock.On("ConditionQueuePop", ctx, conditionKind, serverID)}
+func (_e *MockQueryor_Expecter) ConditionQuery(ctx interface{}, serverID interface{}) *MockQueryor_ConditionQuery_Call {
+	return &MockQueryor_ConditionQuery_Call{Call: _e.mock.On("ConditionQuery", ctx, serverID)}
 }
 
-func (_c *MockQueryor_ConditionQueuePop_Call) Run(run func(ctx context.Context, conditionKind condition.Kind, serverID uuid.UUID)) *MockQueryor_ConditionQueuePop_Call {
+func (_c *MockQueryor_ConditionQuery_Call) Run(run func(ctx context.Context, serverID uuid.UUID)) *MockQueryor_ConditionQuery_Call {
 	_c.Call.Run(func(args mock.Arguments) {
-		run(args[0].(context.Context), args[1].(condition.Kind), args[2].(uuid.UUID))
+		run(args[0].(context.Context), args[1].(uuid.UUID))
 	})
 	return _c
 }
 
-func (_c *MockQueryor_ConditionQueuePop_Call) Return(_a0 *types.ServerResponse, _a1 error) *MockQueryor_ConditionQueuePop_Call {
+func (_c *MockQueryor_ConditionQuery_Call) Return(_a0 *types.ServerResponse, _a1 error) *MockQueryor_ConditionQuery_Call {
 	_c.Call.Return(_a0, _a1)
 	return _c
 }
 
-func (_c *MockQueryor_ConditionQueuePop_Call) RunAndReturn(run func(context.Context, condition.Kind, uuid.UUID) (*types.ServerResponse, error)) *MockQueryor_ConditionQueuePop_Call {
+func (_c *MockQueryor_ConditionQuery_Call) RunAndReturn(run func(context.Context, uuid.UUID) (*types.ServerResponse, error)) *MockQueryor_ConditionQuery_Call {
 	_c.Call.Return(run)
 	return _c
 }
 
-// ConditionStatusUpdate provides a mock function with given fields: ctx, conditionKind, serverID, conditionID, controllerID, statusValue, onlyUpdateTimestamp
-func (_m *MockQueryor) ConditionStatusUpdate(ctx context.Context, conditionKind condition.Kind, serverID uuid.UUID, conditionID uuid.UUID, controllerID registry.ControllerID, statusValue *condition.StatusValue, onlyUpdateTimestamp bool) (*types.ServerResponse, error) {
-	ret := _m.Called(ctx, conditionKind, serverID, conditionID, controllerID, statusValue, onlyUpdateTimestamp)
+// ConditionStatusUpdate provides a mock function with given fields: ctx, conditionKind, serverID, conditionID, statusValue, onlyUpdateTimestamp
+func (_m *MockQueryor) ConditionStatusUpdate(ctx context.Context, conditionKind condition.Kind, serverID uuid.UUID, conditionID uuid.UUID, statusValue *condition.StatusValue, onlyUpdateTimestamp bool) (*types.ServerResponse, error) {
+	ret := _m.Called(ctx, conditionKind, serverID, conditionID, statusValue, onlyUpdateTimestamp)
 
 	if len(ret) == 0 {
 		panic("no return value specified for ConditionStatusUpdate")
@@ -99,19 +96,19 @@ func (_m *MockQueryor) ConditionStatusUpdate(ctx context.Context, conditionKind 
 
 	var r0 *types.ServerResponse
 	var r1 error
-	if rf, ok := ret.Get(0).(func(context.Context, condition.Kind, uuid.UUID, uuid.UUID, registry.ControllerID, *condition.StatusValue, bool) (*types.ServerResponse, error)); ok {
-		return rf(ctx, conditionKind, serverID, conditionID, controllerID, statusValue, onlyUpdateTimestamp)
+	if rf, ok := ret.Get(0).(func(context.Context, condition.Kind, uuid.UUID, uuid.UUID, *condition.StatusValue, bool) (*types.ServerResponse, error)); ok {
+		return rf(ctx, conditionKind, serverID, conditionID, statusValue, onlyUpdateTimestamp)
 	}
-	if rf, ok := ret.Get(0).(func(context.Context, condition.Kind, uuid.UUID, uuid.UUID, registry.ControllerID, *condition.StatusValue, bool) *types.ServerResponse); ok {
-		r0 = rf(ctx, conditionKind, serverID, conditionID, controllerID, statusValue, onlyUpdateTimestamp)
+	if rf, ok := ret.Get(0).(func(context.Context, condition.Kind, uuid.UUID, uuid.UUID, *condition.StatusValue, bool) *types.ServerResponse); ok {
+		r0 = rf(ctx, conditionKind, serverID, conditionID, statusValue, onlyUpdateTimestamp)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*types.ServerResponse)
 		}
 	}
 
-	if rf, ok := ret.Get(1).(func(context.Context, condition.Kind, uuid.UUID, uuid.UUID, registry.ControllerID, *condition.StatusValue, bool) error); ok {
-		r1 = rf(ctx, conditionKind, serverID, conditionID, controllerID, statusValue, onlyUpdateTimestamp)
+	if rf, ok := ret.Get(1).(func(context.Context, condition.Kind, uuid.UUID, uuid.UUID, *condition.StatusValue, bool) error); ok {
+		r1 = rf(ctx, conditionKind, serverID, conditionID, statusValue, onlyUpdateTimestamp)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -129,16 +126,15 @@ type MockQueryor_ConditionStatusUpdate_Call struct {
 //   - conditionKind condition.Kind
 //   - serverID uuid.UUID
 //   - conditionID uuid.UUID
-//   - controllerID registry.ControllerID
 //   - statusValue *condition.StatusValue
 //   - onlyUpdateTimestamp bool
-func (_e *MockQueryor_Expecter) ConditionStatusUpdate(ctx interface{}, conditionKind interface{}, serverID interface{}, conditionID interface{}, controllerID interface{}, statusValue interface{}, onlyUpdateTimestamp interface{}) *MockQueryor_ConditionStatusUpdate_Call {
-	return &MockQueryor_ConditionStatusUpdate_Call{Call: _e.mock.On("ConditionStatusUpdate", ctx, conditionKind, serverID, conditionID, controllerID, statusValue, onlyUpdateTimestamp)}
+func (_e *MockQueryor_Expecter) ConditionStatusUpdate(ctx interface{}, conditionKind interface{}, serverID interface{}, conditionID interface{}, statusValue interface{}, onlyUpdateTimestamp interface{}) *MockQueryor_ConditionStatusUpdate_Call {
+	return &MockQueryor_ConditionStatusUpdate_Call{Call: _e.mock.On("ConditionStatusUpdate", ctx, conditionKind, serverID, conditionID, statusValue, onlyUpdateTimestamp)}
 }
 
-func (_c *MockQueryor_ConditionStatusUpdate_Call) Run(run func(ctx context.Context, conditionKind condition.Kind, serverID uuid.UUID, conditionID uuid.UUID, controllerID registry.ControllerID, statusValue *condition.StatusValue, onlyUpdateTimestamp bool)) *MockQueryor_ConditionStatusUpdate_Call {
+func (_c *MockQueryor_ConditionStatusUpdate_Call) Run(run func(ctx context.Context, conditionKind condition.Kind, serverID uuid.UUID, conditionID uuid.UUID, statusValue *condition.StatusValue, onlyUpdateTimestamp bool)) *MockQueryor_ConditionStatusUpdate_Call {
 	_c.Call.Run(func(args mock.Arguments) {
-		run(args[0].(context.Context), args[1].(condition.Kind), args[2].(uuid.UUID), args[3].(uuid.UUID), args[4].(registry.ControllerID), args[5].(*condition.StatusValue), args[6].(bool))
+		run(args[0].(context.Context), args[1].(condition.Kind), args[2].(uuid.UUID), args[3].(uuid.UUID), args[4].(*condition.StatusValue), args[5].(bool))
 	})
 	return _c
 }
@@ -148,7 +144,7 @@ func (_c *MockQueryor_ConditionStatusUpdate_Call) Return(_a0 *types.ServerRespon
 	return _c
 }
 
-func (_c *MockQueryor_ConditionStatusUpdate_Call) RunAndReturn(run func(context.Context, condition.Kind, uuid.UUID, uuid.UUID, registry.ControllerID, *condition.StatusValue, bool) (*types.ServerResponse, error)) *MockQueryor_ConditionStatusUpdate_Call {
+func (_c *MockQueryor_ConditionStatusUpdate_Call) RunAndReturn(run func(context.Context, condition.Kind, uuid.UUID, uuid.UUID, *condition.StatusValue, bool) (*types.ServerResponse, error)) *MockQueryor_ConditionStatusUpdate_Call {
 	_c.Call.Return(run)
 	return _c
 }
@@ -272,67 +268,6 @@ func (_c *MockQueryor_ConditionTaskQuery_Call) Return(_a0 *types.ServerResponse,
 }
 
 func (_c *MockQueryor_ConditionTaskQuery_Call) RunAndReturn(run func(context.Context, condition.Kind, uuid.UUID) (*types.ServerResponse, error)) *MockQueryor_ConditionTaskQuery_Call {
-	_c.Call.Return(run)
-	return _c
-}
-
-// ControllerCheckin provides a mock function with given fields: ctx, serverID, conditionID, controllerID
-func (_m *MockQueryor) ControllerCheckin(ctx context.Context, serverID uuid.UUID, conditionID uuid.UUID, controllerID registry.ControllerID) (*types.ServerResponse, error) {
-	ret := _m.Called(ctx, serverID, conditionID, controllerID)
-
-	if len(ret) == 0 {
-		panic("no return value specified for ControllerCheckin")
-	}
-
-	var r0 *types.ServerResponse
-	var r1 error
-	if rf, ok := ret.Get(0).(func(context.Context, uuid.UUID, uuid.UUID, registry.ControllerID) (*types.ServerResponse, error)); ok {
-		return rf(ctx, serverID, conditionID, controllerID)
-	}
-	if rf, ok := ret.Get(0).(func(context.Context, uuid.UUID, uuid.UUID, registry.ControllerID) *types.ServerResponse); ok {
-		r0 = rf(ctx, serverID, conditionID, controllerID)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*types.ServerResponse)
-		}
-	}
-
-	if rf, ok := ret.Get(1).(func(context.Context, uuid.UUID, uuid.UUID, registry.ControllerID) error); ok {
-		r1 = rf(ctx, serverID, conditionID, controllerID)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
-// MockQueryor_ControllerCheckin_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ControllerCheckin'
-type MockQueryor_ControllerCheckin_Call struct {
-	*mock.Call
-}
-
-// ControllerCheckin is a helper method to define mock.On call
-//   - ctx context.Context
-//   - serverID uuid.UUID
-//   - conditionID uuid.UUID
-//   - controllerID registry.ControllerID
-func (_e *MockQueryor_Expecter) ControllerCheckin(ctx interface{}, serverID interface{}, conditionID interface{}, controllerID interface{}) *MockQueryor_ControllerCheckin_Call {
-	return &MockQueryor_ControllerCheckin_Call{Call: _e.mock.On("ControllerCheckin", ctx, serverID, conditionID, controllerID)}
-}
-
-func (_c *MockQueryor_ControllerCheckin_Call) Run(run func(ctx context.Context, serverID uuid.UUID, conditionID uuid.UUID, controllerID registry.ControllerID)) *MockQueryor_ControllerCheckin_Call {
-	_c.Call.Run(func(args mock.Arguments) {
-		run(args[0].(context.Context), args[1].(uuid.UUID), args[2].(uuid.UUID), args[3].(registry.ControllerID))
-	})
-	return _c
-}
-
-func (_c *MockQueryor_ControllerCheckin_Call) Return(_a0 *types.ServerResponse, _a1 error) *MockQueryor_ControllerCheckin_Call {
-	_c.Call.Return(_a0, _a1)
-	return _c
-}
-
-func (_c *MockQueryor_ControllerCheckin_Call) RunAndReturn(run func(context.Context, uuid.UUID, uuid.UUID, registry.ControllerID) (*types.ServerResponse, error)) *MockQueryor_ControllerCheckin_Call {
 	_c.Call.Return(run)
 	return _c
 }
